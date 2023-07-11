eb79d00acbab7eaefc2b5dde8405ed18f95dd062
mrcnrzll <139191573+mrcnrzll@users.noreply.github.com>
2023-07-10 22:49:58 -0700

    Add files via upload

---
+++ lang-fr.js
@@ -0,0 +1,83 @@
+chatLangCallback({
+  "APPLICATION_TYPE": "Chat",
+  "S_PAGE_TITLE": "Bavarder",
+  "S_CLOSE": "Fermer",
+  "S_PROVIDER": "<span class='provider-text'>Provided by <span class='provider-five'>Five</span><span class='provider-nine'>9</span></span>",
+  "S_NO_NAME": "Moi",
+  "S_INITIALIZATION_PAGE": {
+    "S_CONTENT": "Initialisation..."
+  },
+  "S_INFORMATION_PAGE": {
+    "S_PROFILE_LABEL": "Profil",
+    "S_NAME_LABEL": "Prénom",
+    "S_EMAIL_LABEL": "Adresse électronique",
+    "S_QUESTION_LABEL": "Question",
+    "S_START_BUTTON": "Démarrer la discussion",
+    "S_SUBJECT_LABEL": "Assujettir",
+    "S_CONTENT_LABEL": "Contenu",
+    "S_SEND_EMAIL_BUTTON": "Envoyer un email"
+  },
+  "S_CONNECTING_PAGE": {
+    "S_CONTENT": "Vous êtes connecté à un agent."
+  },
+  "S_CONVERSATION_PAGE": {
+    "S_SEND_BUTTON": "Envoyer",
+    "S_EXIT_CONFERENCE": "Agent {0} a quitté la conférence.",
+    "S_TRANSFER_GROUP": "Vous avez été transféré au groupe {0}}.",
+    "S_AGENT_ADDED":"Agent ajouté {0}.",
+    "S_AGENT_END":"L'agent a mis fin à la conversation.",
+    "S_CUSTOMER_END": "Le client a mis fin à la conversation.",
+    "S_THANK_YOU_END": "Je vous remercie.",
+    "S_THANK_YOU_OR":"ou",
+    "S_CLOSE_WINDOW":"Fermer la fenêtre",
+    "S_START_NEW": "Commencer un nouveau chat",
+    "S_TYPING": "Dactylographie...",
+    "S_VIDEO_SESSION_REQUEST_RECEIVED": "Demande de session vidéo et / ou audio reçue",
+    "S_VIDEO_SESSION_CANCELED": "Session vidéo et/ou audio annulée",
+    "S_VIDEO_SESSION_REJECTED": "Session vidéo et/ou audio rejetée",
+    "S_VIDEO_SESSION_ACCEPTED": "Session vidéo et/ou audio acceptée",
+    "S_VIDEO_ACTIVATED": "Activation vidéo et/ou audio",
+    "S_VIDEO_TERMINATED": "Vidéo et/ou audio terminé",
+    "S_INPUT_MESSAGE_PLACEHOLDER": "Écrivez ici..."
+  },
+  "S_ERROR_PAGE": {
+    "S_START_OVER": "Recommencer",
+    "S_SEND_EMAIL": "Envoyer un email"
+  },
+  "S_TERMINATE_POPUP": {
+    "S_TERMINATE_BUTTON": "Fin",
+    "S_TERMINATE_CONFIRM": "Êtes-vous sûr de vouloir mettre fin au chat?"
+  },
+  "S_ERRORS": {
+    "S_CONNECTING": "Nous rencontrons des problèmes de connexion. Réessayez plus tard.",
+    "S_WEB_SOCKET": "La version actuelle de votre navigateur n'est pas supportée.<br /> <br />Téléchargez la version la plus récente.",
+    "S_LOCAL_STORAGE": "La version actuelle de votre navigateur n'est pas supportée.<br /><br />Téléchargez la version la plus récente.",
+    "S_LOCAL_STORAGE_SAFARI": "La version actuelle de votre navigateur n'est pas supportée.(ou vous utilisez une fenêtre privée dans Safari).<br /><br />Téléchargez la version la plus récente.",
+    "S_BEFORE_UNLOAD": "Si vous rechargez cette page, vous quitterez la session en cours.",
+    "S_NO_SERVICE": "Aucun agent n'est actuellement disponible. Réessayez plus tard ou envoyez un email.",
+    "S_NETWORK_LOST": "La connexion réseau a été perdue. Vérifiez votre connexion réseau ou réessayez plus tard."
+  },
+  "S_MESSAGES": {
+    "S_TO": "L'agent {0} est en train de discuter avec vous.",
+    "S_FROM": "L'agent {0} a transféré le chat à {1}.",
+    "S_GR_IN_PROGRESS": "Nous rencontrons des problèmes de réseau. Le problème peut durer jusqu'à 10 minutes. Notre agent répondra à votre message dès que possible. Merci pour votre patience."
+  },
+  "S_END_CHAT": "Terminer le chat",
+  "S_CANCEL": "Annuler",
+  "S_VALIDATE": {
+    "S_REQUIRED": "champs requis",
+    "S_INVALID_EMAIL": "Adresse e-mail invalide"
+  },
+  "S_VIDEO_POPUP": {
+    "S_ACCEPT": "Acceptez",
+    "S_REJECT": "Rejeter",
+    "S_MESSAGE": "Session vidéo et / ou audio demandée"
+  },
+  "S_SENT_PAGE": {
+    "S_CONTENT": "Merci pour votre e-mail. Nous vous contacterons dès que possible.",
+    "S_START_NEW": "Commencer un nouveau e-mail"
+  },
+  "S_WAITING_PAGE": {
+    "S_CONTENT": "Veuillez patienter pendant que nous cherchons un associé"
+  }
+});
